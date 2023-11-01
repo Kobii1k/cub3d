61aac633b187a6935eaf202ef5b913e1a493a6f1
Maxens Gagne <mgagne@z3r7p5.42lyon.fr>
2023-11-01 18:57:28 +0100

    norm minimap

--- srcs/game/minimap.c
+++ srcs/game/minimap.c
@@ -6,7 +6,7 @@
 /*   By: mgagne <mgagne@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 12:13:41 by mgagne            #+#    #+#             */
-/*   Updated: 2023/11/01 18:45:25 by mgagne           ###   ########.fr       */
+/*   Updated: 2023/11/01 18:48:00 by mgagne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,11 @@ void	draw_minimap(t_data *cube)
 		x = xkeep - 200;
 		while (x <= xkeep)
 		{
-			if (x >= 0 && y >= 0 && floor(y / 10) < cube->height && cube->map[(int)floor(y / 10)][(int)floor(x / 10)])
-				put_minimap(cube, (floor(x - xkeep + 200)), (floor(y - ykeep + 200)), cube->map[(int)floor((y / 10))][(int)floor((x / 10))]);
+			if (x >= 0 && y >= 0 && floor(y / 10) < cube->height
+				&& cube->map[(int)floor(y / 10)][(int)floor(x / 10)])
+				put_minimap(cube, (floor(x - xkeep + 200)),
+					(floor(y - ykeep + 200)),
+					cube->map[(int)floor((y / 10))][(int)floor((x / 10))]);
 			x += 0.99999;
 		}
 		y += 0.99999;
