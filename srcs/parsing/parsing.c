495c0cabb37a02b824058de4c17db6bd348e20ef
Maxens Gagne <mgagne@z2r10p4.42lyon.fr>
2023-11-13 15:22:53 +0100

    leaks map create

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mgagne <mgagne@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 15:36:14 by mgagne            #+#    #+#             */
-/*   Updated: 2023/11/10 13:05:52 by mgagne           ###   ########.fr       */
+/*   Updated: 2023/11/13 15:22:01 by mgagne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,7 @@ int	create_map(int fd, t_data *cube)
 			if (ft_isspace(str[0]) || str[0] == '1')
 				break ;
 			else
-				return (ft_printf("map error : unexpected value\n"), 1);
+				return (free(str), ft_printf("map error : unexpected value\n"), 1);
 		}
 		free(str);
 		str = get_next_line(fd);
