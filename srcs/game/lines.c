bfb8adcf3932fbef5d49a470e9a8928ca6ee37b6
42Colins <cprojean@icloud.com>
2023-11-13 13:40:55 +0100

    invalid reads handled

--- srcs/game/lines.c
+++ srcs/game/lines.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   lines.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgagne <mgagne@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: cprojean <cprojean@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 11:24:58 by cprojean          #+#    #+#             */
-/*   Updated: 2023/11/10 13:01:40 by mgagne           ###   ########.fr       */
+/*   Updated: 2023/11/13 13:40:22 by cprojean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3d.h"
+
+void	init_tab(int tab[35][2]);
 
 int	ft_reduce_opacity(int color)
 {
@@ -72,6 +74,8 @@ void	draw_vision(t_data *cube)
 
 	tmp_angle = cube->j1.player_angle;
 	tmp_angle = tmp_angle - 30;
+	init_tab(tab);
+	init_tab(oldtab);
 	do_line(cube, tmp_angle, tab, oldtab);
 	tmp_angle += 1;
 	while (tmp_angle < cube->j1.player_angle + 30)
@@ -87,4 +91,17 @@ void	draw_vision(t_data *cube)
 		tmp_angle += 0.1;
 	}
 	return ;
+}
+
+void	init_tab(int tab[35][2])
+{
+	int	index;
+
+	index = 0;
+	while (index < 35)
+	{
+		tab[index][0] = -1;
+		tab[index][1] = -1;
+		index++;
+	}
 }
